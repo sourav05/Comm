ee9985498a9f4ae36f17c7a5fa65e9cd50d84096
sourav05 <40533228+sourav05@users.noreply.github.com>
2018-12-18 01:29:17 +0530

    service_2

--- addservice/src/main/java/com/example/addservice/AddController.java
+++ addservice/src/main/java/com/example/addservice/AddController.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AddController {
@@ -12,9 +13,17 @@ public class AddController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 	
+	@Autowired
+	private RestTemplate template;
+	
 	@GetMapping(value="add/{num1}/{num2}")
 	public String add(@PathVariable(value="num1") int number1, @PathVariable(value="num2") int number2){
-		System.out.println("Service Registered : " + discoveryClient.getInstances("add-service").get(0));
-		return String.valueOf(number1+number2);
+		/*System.out.println("Service Registered : " + discoveryClient.getInstances("add-service").get(0));
+		return String.valueOf(number1+number2);*/
+		System.out.println("Service Registered : " + discoveryClient.getInstances("add-service").get(0)
+				.getUri()+"/add/"+number1+"/"+number2);
+		ResponseEntity<String> response = template.getForEntity(discoveryClient.getInstances("add-service").get(0)
+																.getUri()+"/add/"+number1+"/"+number2, String.class);	
+		return response.getBody();
 	}
 }
