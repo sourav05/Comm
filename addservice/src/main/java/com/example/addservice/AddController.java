b94938de5b3394b3898b8d404af0f1684e241d3c
sourav05 <40533228+sourav05@users.noreply.github.com>
2018-12-18 00:58:47 +0530

    init

--- addservice/src/main/java/com/example/addservice/AddController.java
+++ addservice/src/main/java/com/example/addservice/AddController.java
@@ -1,5 +1,7 @@
 package com.example.addservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AddController {
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
 	@GetMapping(value="add/{num1}/{num2}")
 	public String add(@PathVariable(value="num1") int number1, @PathVariable(value="num2") int number2){
+		System.out.println("Service Registered : " + discoveryClient.getInstances("add-service").get(0));
 		return String.valueOf(number1+number2);
 	}
 }
